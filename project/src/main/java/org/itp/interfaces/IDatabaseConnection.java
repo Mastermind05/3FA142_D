71d800f94c28d39ed772c35ff4a42ea6a9761eb6
DESKTOP-T54I6IB\huber <hubernicoicloud.com>
2024-10-23 09:30:28 +0200

    #13: Erfolgreiche Implementierung der openconnection methode und des zugehörigen Junit Tests

--- project/src/main/java/org/itp/interfaces/IDatabaseConnection.java
+++ project/src/main/java/org/itp/interfaces/IDatabaseConnection.java
@@ -1,10 +1,11 @@
 package org.itp.interfaces;
 
+import java.sql.SQLException;
 import java.util.Properties;
 
 public interface IDatabaseConnection {
 
-    IDatabaseConnection openConnection(Properties properties);
+    IDatabaseConnection openConnection(Properties properties) throws SQLException;
 
     void createAllTables();
 
