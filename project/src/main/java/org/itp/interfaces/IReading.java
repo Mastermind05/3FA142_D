ed3d77aaa2683be2f74cf0beb55c22273a92b21c
DESKTOP-T54I6IB\huber <hubernicoicloud.com>
2024-11-06 08:59:24 +0100

    #28: Extenden der Interfaces IReading und ICustomer erstellen der Tables Enum

--- project/src/main/java/org/itp/interfaces/IReading.java
+++ project/src/main/java/org/itp/interfaces/IReading.java
@@ -1,11 +1,10 @@
 package org.itp.interfaces;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import org.itp.enums.KindOfMeter;
 
-public interface IReading {
+public interface IReading extends IID{
     void setComment(String comment);
     void setCustomer(ICustomer customer);
     void setDateOfReading(LocalDate dateOfReading);
