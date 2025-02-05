53f59e1f4fe78f66c4996b2a0fbd9441e5232c4e
HuberNico <hubernicoicloud.com>
2025-02-05 11:54:46 +0100

    #64: Hinzufügen der Customer Tabelle

--- project/src/main/Angular/boilerplate/src/app/home/home.module.ts
+++ project/src/main/Angular/boilerplate/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { StartseiteRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CustomertableComponent } from '../customertable/customertable.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     StartseiteRoutingModule,
-    SharedModule
+    SharedModule,
+    CustomertableComponent
   ]
 })
 export class StartseiteModule { }
