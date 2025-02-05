47005fc975e925f27c87c5305e9d30c28fb2b688
HuberNico <hubernicoicloud.com>
2025-02-05 11:38:07 +0100

    #64: Erstellen der Buttons und Methoden sowie der Dateninterfaces

--- project/src/main/Angular/boilerplate/src/app/home/home/home.component.ts
+++ project/src/main/Angular/boilerplate/src/app/home/home/home.component.ts
@@ -13,16 +13,26 @@ export interface PeriodicElement {
 }
 
 export interface Customer {
-  id: number;
+  id: string;
   firstName: string;
   lastName: string;
-  birthdate: string;
+  birthDate: string;
   gender: string;
 }
 
+export interface Reading {
+  id: string;
+  dateOfReading: string;
+  kindOfMeter: string;
+  meterCount: string;
+  meterId: string;
+  substitute: string;
+}
+
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, dateOfReading: 'test', kindOfMeter: 'test', meterCount: 'test', meterId: 'test', substitute: 'test'},
-];
+  ];
+
+const baseurl = 'http://localhost:8080/test/ressources/';
 
 @Component({
   selector: 'app-home',
@@ -32,25 +42,111 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 
-
 export class HomeComponent {
   displayedColumns: string[] = ['position', 'dateofreading', 'kindofmeter', 'metercount', 'meterid', 'substitute'];
   dataSource = ELEMENT_DATA;
   customer: Customer = {
-    id: 0,
+    id: '',
     firstName: '',
     lastName: '',
-    birthdate: '',
+    birthDate: '',
     gender: ''
   };
+  reading: Reading = {
+    id: '',
+    dateOfReading: '',
+    kindOfMeter: '',
+    meterCount: '',
+    meterId: '',
+    substitute: ''
+  };
+  
   addCustomer() {
-    const url = 'http://localhost:8080/test/ressources/customers'; // Ersetzen Sie dies durch Ihre API-URL
-    axios.post<Customer>(url, this.customer)
+    const url = `${baseurl}customers`; 
+      axios.post<Customer>(url, this.customer)
       .then(response => {
         console.log('Customer added successfully', response.data);
       })
       .catch(error => {
         console.error('Error adding customer', error);
       });
+      console.log(this.customer);
+  }
+  deleteCustomer() {
+    const url = `${baseurl}customers/${this.customer.id}`;
+    axios.delete<Customer>(url)
+      .then(response => {
+        console.log('Customer deletet successfully', response.data);
+      })
+      .catch(error => {
+        console.error('Error deleting customer', error);
+      });
+      console.log(this.customer.id);
+  }
+  updateCustomer() {
+    const url = `${baseurl}customers`; 
+    axios.put<Customer>(url, this.customer)
+      .then(response => {
+        console.log('Customer updated successfully', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating customer', error);
+      });
+      console.log(this.customer);
+  }
+  getCustomer() {
+    const url = `${baseurl}customers/${this.customer.id}`;
+    axios.get<Customer>(url)
+      .then(response => {
+        console.log('Found customer successfully', response.data);
+      })
+      .catch(error => {
+        console.error('There is no customer with this UUID', error);
+      });
+      console.log(this.customer);
+  }
+  addReading() {
+    const url = `${baseurl}readings`; 
+    axios.post<Reading>(url, this.reading)
+      .then(response => {
+        console.log('Reading created successfully', response.data);
+      })
+      .catch(error => {
+        console.error('Error creating customer', error);
+      });
+      console.log(this.customer);
+  }
+  getReading() {
+    const url = `${baseurl}customers/${this.reading.id}`; 
+    axios.get<Reading>(url)
+      .then(response => {
+        console.log('Reading updated successfully', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating reading', error);
+      });
+      console.log(this.customer);
+  }
+  updateReading() {
+    const url = `${baseurl}readings`; 
+    axios.put<Reading>(url, this.reading)
+      .then(response => {
+        console.log('Customer updated successfully', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating customer', error);
+      });
+      console.log(this.customer);
+  }
+  deleteReading() {
+    const url = `${baseurl}readings/${this.reading.id}`; 
+    axios.delete<Customer>(url)
+      .then(response => {
+        console.log('Customer updated successfully', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating customer', error);
+      });
+      console.log(this.customer);
   }
 }
